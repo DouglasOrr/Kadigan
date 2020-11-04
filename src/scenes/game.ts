bfe5ea6654e1f34a68116534ddfe264de4823692
Douglas Orr <douglas.orr99@gmail.com>
2020-11-04 23:21:34 +0000

    Make a smaller selectable ship

--- src/scenes/game.ts
+++ src/scenes/game.ts
@@ -14,11 +14,25 @@ export default class GameScene extends Phaser.Scene {
     }
     create(): void {
         this.ship = this.add.sprite(100, 200, "ship");
-        this.ship.setScale(0.5, 0.5);
+        this.ship.setScale(0.25, 0.25);
         this.physics.add.existing(this.ship, false);
 
-        this.input.on("pointerdown", (e) => {
-            this.target.copy(e.position);
+        this.input.on("pointerdown", (pointer) => {
+            if (pointer.leftButtonDown()) {
+                this.ship.setData("selected", false);
+                this.ship.setTint(0xffffff);
+            }
+            if (pointer.rightButtonDown() && this.ship.getData("selected")) {
+                this.target.copy(pointer.position);
+            }
+        });
+
+        this.ship.setInteractive().on("pointerdown", (pointer, _x, _y, event) => {
+            if (pointer.leftButtonDown()) {
+                this.ship.setData("selected", true);
+                this.ship.setTint(0xffff00);
+                event.stopPropagation();
+            }
         });
     }
     update(): void {
