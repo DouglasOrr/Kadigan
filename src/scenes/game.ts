e197d5b3258e0747b3e00024df0969972dd86a4b
Douglas Orr <douglas.orr99@gmail.com>
2020-11-06 21:19:45 +0000

    Refactor behaviour into Ship class

--- src/scenes/game.ts
+++ src/scenes/game.ts
@@ -1,62 +1,77 @@
 import Phaser from "phaser";
 
+enum ShipState {
+    Idle,
+    Moving
+}
+const MoveFinishThreshold = 1.0;
+const ShipSpeed = 50.0;
+
 class Ship extends Phaser.GameObjects.Sprite {
+    state: ShipState;
+    target: Phaser.Math.Vector2;
     selected: boolean;
 
     constructor(scene, x, y) {
         super(scene, x, y, "ship");
         this.setScale(0.25, 0.25);
+        this.state = ShipState.Idle;
+        this.target = new Phaser.Math.Vector2();
         this.selected = false;
     }
     select(selected: boolean) {
         this.setTint(selected ? 0xffff00 : 0xffffff);
         this.selected = selected;
     }
+    move(target: Phaser.Math.Vector2) {
+        this.state = ShipState.Moving;
+        this.target.copy(target);
+    }
+    update() {
+        if (this.state == ShipState.Moving) {
+            const body = <Phaser.Physics.Arcade.Body>this.body;
+            body.velocity.copy(this.target).subtract(body.center);
+            if (body.velocity.length() < MoveFinishThreshold) {
+                this.state = ShipState.Idle;
+                body.velocity.reset();
+            } else {
+                body.velocity.normalize().scale(ShipSpeed);
+            }
+        }
+    }
 }
 
 export default class GameScene extends Phaser.Scene {
-    target: Phaser.Math.Vector2;
     ship: Ship;
 
     constructor() {
         super("game");
-        this.target = new Phaser.Math.Vector2();
-        this.target.set(undefined, undefined);
     }
     preload(): void {
         this.load.image("ship", "/assets/ship0.png");
     }
     create(): void {
+        this.input.on(Phaser.Input.Events.POINTER_DOWN, this.onpointerdown, this);
+
         this.ship = new Ship(this, 100, 200);
         this.add.existing(this.ship);
         this.physics.add.existing(this.ship, false);
-
-        this.input.on("pointerdown", (pointer) => {
-            if (pointer.leftButtonDown()) {
-                const selected = this.physics.overlapCirc(pointer.position.x, pointer.position.y, 0);
-                if (selected.length) {
-                    const ship = <Ship>selected[0].gameObject;
-                    ship.select(true);
-                    return;
-                }
-                this.ship.select(false);
+    }
+    onpointerdown(pointer: Phaser.Input.Pointer): void {
+        if (pointer.leftButtonDown()) {
+            const selected = this.physics.overlapCirc(pointer.position.x, pointer.position.y, 0);
+            if (selected.length) {
+                const ship = <Ship>selected[0].gameObject;
+                ship.select(true);
+                return;
             }
-            if (pointer.rightButtonDown() && this.ship.selected) {
-                this.target.copy(pointer.position);
-            }
-        });
+            this.ship.select(false);
+        }
+        if (pointer.rightButtonDown() && this.ship.selected) {
+            this.ship.move(pointer.position);
+        }
     }
     update(): void {
-        if (this.target.x !== undefined) {
-            const body = <Phaser.Physics.Arcade.Body>this.ship.body;
-            const offset = this.target.clone().subtract(body.center);
-            if (offset.length() < 1) {
-                this.target.set(undefined, undefined);
-                body.velocity.reset();
-            } else {
-                const velocity = offset.normalize().scale(50);
-                body.velocity.copy(velocity);
-            }
-        }
+        this.ship.update();
     }
 }
